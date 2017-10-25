8325c994d8c4266f7ad2595e6b2c3dc255c0e12d
AmazeLee <911711054@qq.com>
2017-10-25 14:07:58 +0800

    Hibernate_day01

---
+++ src/com/sh/a_hello/Demo.java
@@ -0,0 +1,34 @@
+package com.sh.a_hello;
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.junit.Test;
+
+import com.sh.domain.Customer;
+
+/**
+ * 测试Hibernate框架
+ * @author Administrator
+ *
+ */
+public class Demo {
+	//保存客户
+	@Test
+	public void fun1() {
+		Configuration conf = new Configuration().configure();
+		SessionFactory sessionFactory = conf.buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		Transaction tx = session.beginTransaction();
+		
+		Customer c = new Customer();
+		c.setCust_name("百度公司");
+		session.save(c);
+		
+		tx.commit();
+		session.close();
+		sessionFactory.close();
+	}
+}
